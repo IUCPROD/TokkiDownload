3caa4dc6906a30206178f99705e058f123a8158a
IUCPROD <38842440+IUCPROD@users.noreply.github.com>
2019-10-26 08:49:37 +0200

    Delete setup.py

--- Chrome/Windows/setup.py
+++
@@ -1,7 +0,0 @@
-import os
-directory = os.getcwd()
-
-key = directory + "\\manifest.json"
-os.system("reg add HKCU\\SOFTWARE\\Google\\Chrome\\NativeMessagingHosts /f")
-cmd = "reg add HKCU\\SOFTWARE\\Google\\Chrome\\NativeMessagingHosts\\tkdl /t REG_SZ /f /d "+ key
-os.system(cmd)
