ad61c873c8a3f07943b6fc3ac0ff0f652ba60624
IUCPROD <38842440+IUCPROD@users.noreply.github.com>
2019-10-26 09:00:00 +0200

    Add files via upload

---
+++ Firefox/Windows/setup.py
@@ -0,0 +1,11 @@
+import os
+directory = os.getcwd()
+
+key = directory + "\\manifest.json"
+print("Updating Registry NativeMessagingHosts")
+os.system("reg add HKCU\\SOFTWARE\\Mozilla\\NativeMessagingHosts /f")
+cmd = "reg add HKCU\\SOFTWARE\\Mozilla\\NativeMessagingHosts\\tkdl /t REG_SZ /f /d "+ key
+print("Updating Registry tdkl")
+os.system(cmd)
+print("Updating Youtube-dl")
+os.system(directory + "\\youtube-dl.exe -U")
