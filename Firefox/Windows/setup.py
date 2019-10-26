955aa5e31ed1f3030b80620ffd451e420a4cd3ca
IUCPROD <38842440+IUCPROD@users.noreply.github.com>
2019-10-26 08:54:39 +0200

    Delete setup.py

--- Firefox/Windows/setup.py
+++
@@ -1,8 +0,0 @@
-import os
-directory = os.getcwd()
-
-key = directory + "\\manifest.json"
-print(key)
-os.system("reg add HKCU\\SOFTWARE\\Mozilla\\NativeMessagingHosts /f")
-cmd = "reg add HKCU\\SOFTWARE\\Mozilla\\NativeMessagingHosts\\tkdl /t REG_SZ /f /d "+ key
-os.system(cmd)
